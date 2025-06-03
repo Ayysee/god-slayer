7af04c363b600b2a5aa2e10804b84617159f7928
Franco Logozzo <franco.logozzo@gmail.com>
2025-06-02 22:19:50 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,30 @@
+loops.forever(function () {
+    mobs.spawnParticle(EGG_DESTROY, pos(0, 10, 0))
+})
+loops.forever(function () {
+    mobs.spawnParticle(EGG_DESTROY, pos(0, 13, 0))
+})
+loops.forever(function () {
+    mobs.spawnParticle(EGG_DESTROY, pos(0, 16, 0))
+})
+loops.forever(function () {
+    mobs.spawnParticle(EGG_DESTROY, pos(0, 19, 0))
+})
+loops.forever(function () {
+    mobs.spawnParticle(EGG_DESTROY, pos(-3, 19, 0))
+})
+loops.forever(function () {
+    mobs.spawnParticle(EGG_DESTROY, pos(3, 19, 0))
+})
+loops.forever(function () {
+    mobs.spawnParticle(EGG_DESTROY, pos(0, 22, 0))
+})
+loops.forever(function () {
+    mobs.spawnParticle(EGG_DESTROY, pos(0, 25, 0))
+})
+loops.forever(function () {
+    mobs.spawnParticle(EGG_DESTROY, pos(0, 28, 0))
+})
+loops.forever(function () {
+    mobs.spawnParticle(EGG_DESTROY, pos(0, 31, 0))
+})
